8936b63a1d9a56d3081776363f985d6e434249bf
reveny <113244907+reveny@users.noreply.github.com>
2026-05-04 17:08:45 +0200

    chore(release): bump version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.reveny.habittracker"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 110
+        versionName = "1.1.0"
     }
 
     buildTypes {
